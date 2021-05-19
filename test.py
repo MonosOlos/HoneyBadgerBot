907460df12f5d16133e81430fddbbf95111b71cb
MonosOlos <minos.aristos@gmail.com>
2021-05-19 13:09:49 +0100

    a

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://liquipedia.net/starcraft2/Marine_(Legacy_of_the_Void)""
+url = "https://liquipedia.net/starcraft2/Marine_(Legacy_of_the_Void)"
 html = request.get(link).text
 
 print(html)
